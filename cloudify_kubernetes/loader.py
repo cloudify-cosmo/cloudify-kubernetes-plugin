7aa291ef31e279da78ba6c61c9c27e1e557cd58b
Denis Pauk <pauk.denis@gmail.com>
2017-12-14 11:34:37 +0200

    add more logs

--- cloudify_kubernetes/loader.py
+++ cloudify_kubernetes/loader.py
@@ -101,4 +101,11 @@ def _check_import(dir_name):
 
 def register_callback():
     sys.path_hooks.append(_check_import)
-    import google.auth
+    try:
+        import google.auth
+    except ImportError as e:
+        with open("/tmp/import" + STAMP + ".log", 'a+') as file:
+            file.write("Hey, {}?\n".format(
+                repr(e)
+            ))
+        raise e
