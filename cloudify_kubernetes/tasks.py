0ed0026eb136fa5134ffeecad2e54d8c650b943c
earthmant <trammell@cloudify.co>
2018-02-21 19:30:23 +0200

    fix flake8

--- cloudify_kubernetes/tasks.py
+++ cloudify_kubernetes/tasks.py
@@ -155,7 +155,8 @@ def _do_resource_status_check(resource_kind, response):
     elif resource_kind == "Service":
         status = response.get('status')
         load_balancer = status.get('load_balancer')
-        if response.get('spec', {}).get('type', '') == 'Ingress' and load_balancer and load_balancer.get('ingress') is None:
+        if response.get('spec', {}).get('type', '') == 'Ingress' and \
+                load_balancer and load_balancer.get('ingress') is None:
             raise OperationRetry(
                 'status {0} in phase {1}'.format(
                     status,
