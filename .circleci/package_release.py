5d838f76ceae77bd827c4fa223c7a435b0cd4561
earthmant <trammell@cloudify.co>
2020-04-02 17:51:55 +0300

    update plugin relase

---
+++ .circleci/package_release.py
@@ -0,0 +1,11 @@
+from os import path, pardir
+from ecosystem_cicd_tools.release import (
+    plugin_release_with_latest, find_version)
+
+setup_py = path.join(
+    path.abspath(path.join(path.dirname(__file__), pardir)),
+    'setup.py')
+
+if __name__ == '__main__':
+    plugin_release_with_latest(
+        'cloudify-kubernetes-plugin', find_version(setup_py))
