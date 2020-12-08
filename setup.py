4d0e04ecec66ff11470f03fc145c960aff5ca9c2
earthmant <trammell@cloudify.co>
2020-12-08 11:21:23 +0200

    bump pyyaml __NODOCS__

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'cloudify-python-importer==0.2',
         'cloudify-common>=4.5',
         'kubernetes==10.0.1',
-        'pyyaml>=3.12',
+        'pyyaml>=4.2b1',
         'pyasn1>=0.1.7',
         'pyasn1-modules>=0.0.5,<0.2.1',
         'oauth2client',  # used only in GCPServiceAccountAuthentication
