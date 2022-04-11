21f2a9234e5018cc34c1eb8ea58b26dab8903293
nely <nely@cloudify.co>
2022-04-11 11:32:17 +0300

    refresh_and_store_token

--- cloudify_kubernetes/workflows.py
+++ cloudify_kubernetes/workflows.py
@@ -15,9 +15,10 @@
 import ast
 import json
 
-from cloudify.decorators import workflow
-from cloudify.exceptions import NonRecoverableError
 from cloudify.workflows import ctx
+from cloudify.decorators import workflow
+from cloudify.manager import get_rest_client
+from cloudify.exceptions import NonRecoverableError
 
 from . import utils
 
@@ -125,3 +126,72 @@ def update_resource_definition(node_instance_id,
         _params={utils.DEFINITION_ADDITIONS: resource_definition_changes})
     node_instance.logger.info(
         'Executed update in order to push the new changes.')
+
+
+def refresh_and_store_token(kubernetes_cluster_node_instance_id,
+                            deployment_capability_name,
+                            service_account_node_instance_id,
+                            secret_token_node_instance_id,
+                            store_token_and_kubeconfig_id):
+
+    client = get_rest_client()
+
+    # cfy exec start execute_operation -p operation=cloudify.interfaces.lifecycle.poststart -p node_instance_ids='["eks_cluster"]' -d eks
+    # cfy exec start execute_operation -vv -p operation=cloudify.interfaces.lifecycle.check_drift -p node_instance_ids='["eks_cluster"]' -d eks
+    cluster_node_instance = get_node_instance_from_node_instance_id_or_node_id(
+        kubernetes_cluster_node_instance_id)
+    execute_node_instance_operation(
+        cluster_node_instance, 'cloudify.interfaces.lifecycle.poststart')
+    execute_node_instance_operation(
+        cluster_node_instance, 'cloudify.interfaces.lifecycle.check_drift')
+
+    # cfy secrets update kubernetes_config -f file
+    kubernetes_config = ctx.deployment.capabilites.get(
+        deployment_capability_name, {})
+    ctx.logger.info('This is the capability: {}'.format(kubernetes_config))
+    client.secrets.create('kubernetes_config', kubernetes_config.get('value'))
+
+    # cfy exec start execute_operation -p node_instance_ids='["new_service_account"]' -p operation=cloudify.interfaces.lifecycle.update -vv -d eks
+    # cfy exec start execute_operation -p node_instance_ids='["new_service_account"]' -p operation=cloudify.interfaces.lifecycle.poststart -vv -d eks
+    service_account_node_instance = \
+        get_node_instance_from_node_instance_id_or_node_id(
+            service_account_node_instance_id)
+    execute_node_instance_operation(service_account_node_instance,
+                                    'cloudify.interfaces.lifecycle.update')
+    execute_node_instance_operation(service_account_node_instance,
+                                    'cloudify.interfaces.lifecycle.poststart')
+
+    # cfy exec start execute_operation -p node_instance_ids='["secret"]' -p operation=cloudify.interfaces.lifecycle.delete -vv -d eks
+    # cfy exec start execute_operation -p node_instance_ids='["secret"]' -p operation=cloudify.interfaces.lifecycle.create -vv -d eks
+    secret_token_node_instance = \
+        get_node_instance_from_node_instance_id_or_node_id(
+            secret_token_node_instance_id)
+    execute_node_instance_operation(secret_token_node_instance,
+                                    'cloudify.interfaces.lifecycle.delete')
+    execute_node_instance_operation(secret_token_node_instance,
+                                    'cloudify.interfaces.lifecycle.creat')
+
+    # cfy exec start execute_operation -p node_instance_ids='["store_token_and_kubeconfig"]' -p operation=cloudify.interfaces.lifecycle.create -vv -d eks
+    store_token_and_kubeconfig_node_instance = \
+        get_node_instance_from_node_instance_id_or_node_id(
+            store_token_and_kubeconfig_id)
+    execute_node_instance_operation(store_token_and_kubeconfig_node_instance,
+                                    'cloudify.interfaces.lifecycle.creat')
+
+
+def get_node_instance_from_node_instance_id_or_node_id(
+        provided_node_instance_id):
+    try:
+        desired_node_instance = ctx.get_node_instance(
+            provided_node_instance_id)
+    except RuntimeError:
+        desired_node_instance = None
+        for node_instance in ctx.get_node_instance:
+            if node_instance.node_id == provided_node_instance_id:
+                desired_node_instance = node_instance
+    if not desired_node_instance:
+        raise NonRecoverableError(
+            'A valid node instance or node ID for a '
+            'X node was not found'
+        )
+
