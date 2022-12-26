b8d872d00be1f5b7f8edc211f83599bcfc81553f
nelynehemia <nely@cloudify.co>
2022-12-26 11:30:48 +0200

    multiple_file_resource_check_drift , resource_check_drift, custom_resource_check_drift, file_resource_check_drift

--- cloudify_kubernetes/tasks/operations.py
+++ cloudify_kubernetes/tasks/operations.py
@@ -391,6 +391,16 @@ def custom_resource_read(client, api_mapping, resource_definition, **kwargs):
 @with_kubernetes_client
 @resource_task(
     retrieve_resource_definition=resource_definition_from_blueprint,
+    retrieve_mapping=mapping_by_data,
+    resource_state_function=_check_if_resource_exists
+)
+def custom_resource_check_drift(client, api_mapping, resource_definition, **kwargs):
+    file_resource_check_drift(client, api_mapping, resource_definition, **kwargs)
+
+
+@with_kubernetes_client
+@resource_task(
+    retrieve_resource_definition=resource_definition_from_blueprint,
     retrieve_mapping=mapping_by_kind,
     resource_state_function=_check_if_resource_exists
 )
@@ -446,20 +456,35 @@ def file_resource_read(client, api_mapping, resource_definition, **kwargs):
 def file_resource_check_drift(client, api_mapping, resource_definition, **kwargs):
     ctx.logger.info('*** in file_resource_check_drift **')
 
-    path = retrieve_path(kwargs)
-    _, resource, _ = retrieve_last_create_path(path, delete=False)
-
     # Read All resources.
     read_response = _do_resource_read(
         client, api_mapping, resource_definition, **kwargs)
 
-    output = ctx.instance.runtime_properties.get(DEFS)
-    read_response = JsonCleanuper(read_response).to_dict()
-    ctx.logger.info('*** read_response: {}'.format(read_response))
-    for source in output:
-        if source.get('kind') == read_response.get('kind'):
+    ctx.logger.info(
+        'Resource definition: {0}'.format(read_response))
+
+    storable_object = KubernetesResourceDefinition(
+            read_response['kind'],
+            read_response.get('api_version', read_response.get('apiVersion')),
+            read_response['metadata']
+        )
+
+    prop = ctx.instance.runtime_properties.get(DEFS)
+    comparable_dict = JsonCleanuper(storable_object).to_dict()
+    kind = comparable_dict.get('kind')
+    name = comparable_dict['metadata'].get('name')
+    namespace = comparable_dict['metadata'].get('namespace')
+
+    for source in prop:
+        if source.get('kind') == kind and \
+                source['metadata'].get('name') == name and \
+                source['metadata'].get('namespace') == namespace:
+
+            ctx.logger.info('*** comparable_dict: {}'.format(comparable_dict))
             ctx.logger.info('*** source: {}'.format(source))
-            ctx.logger.info('*** DeepDiff: {}'.format(DeepDiff(read_response, source)))
+
+            diff = DeepDiff(comparable_dict, source)
+            ctx.logger.info('*** diff: {}'.format(diff))
 
 
 @with_kubernetes_client
@@ -634,6 +659,17 @@ def multiple_file_resource_read(**kwargs):
 
     for file_resource in file_resources:
         file_resource_read(file=file_resource, **kwargs)
+
+
+def multiple_file_resource_check_drift(**kwargs):
+    file_resources = kwargs.get(
+        NODE_PROPERTY_FILES,
+        ctx.node.properties.get(NODE_PROPERTY_FILES, [])
+    )
+    validate_file_resources(file_resources)
+
+    for file_resource in file_resources:
+        file_resource_check_drift(file=file_resource, **kwargs)
 
 
 def multiple_file_resource_delete(**kwargs):
